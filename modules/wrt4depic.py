980bb661327e55438ef873d386bf12bb21d3069b
TheVitalikspirit <SuperVitalik.NEchip@gmail.com>
2023-11-18 14:52:12 +0200

    Update wrt4depic.py

--- modules/wrt4depic.py
+++ modules/wrt4depic.py
@@ -12,10 +12,12 @@ import gradio as gr
 def depic(pic:str, depic:int):
     depictering = None
     hwat = "what?"
-#
+#-----------------------------
 
-def wrt4depic(id_task: str, prompt: str, negative_prompt: str, prompt_styles, steps: int, sampler_name: str, n_iter: int, batch_size: int, cfg_scale: float, height: int, width: int, enable_hr: bool, denoising_strength: float, hr_scale: float, hr_upscaler: str, hr_second_pass_steps: int, hr_resize_x: int, hr_resize_y: int, hr_checkpoint_name: str, hr_sampler_name: str, hr_prompt: str, hr_negative_prompt, override_settings_texts, request: gr.Request, *args):
-    override_settings = create_override_settings_dict(override_settings_texts)
+def wrt4depic(
+    id_task: str, prompt: str, negative_prompt: str, prompt_styles, steps: int, sampler_name: str, n_iter: int, batch_size: int, cfg_scale: float, height: int, width: int, enable_hr: bool, denoising_strength: float, hr_scale: float, hr_upscaler: str, hr_second_pass_steps: int, hr_resize_x: int, hr_resize_y: int, hr_checkpoint_name: str, hr_sampler_name: str, hr_prompt: str, hr_negative_prompt, override_settings_texts, request: gr.Request, *args
+    ):
+    override_settings = create_override_settings_dict(override_settings_texts);
 
     p = processing.StableDiffusionProcessingTxt2Img(
         sd_model=shared.sd_model,
