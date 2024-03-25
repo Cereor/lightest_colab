e5ab41022eaf340be9c2193d76605af1a4190ca0
TheVitalikspirit <SuperVitalik.NEchip@gmail.com>
2024-03-25 09:20:53 +0200

    Update import_hook.py

--- modules/import_hook.py
+++ modules/import_hook.py
@@ -3,3 +3,11 @@ import sys
 # this will break any attempt to import xformers which will prevent stability diffusion repo from trying to use it
 if "--xformers" not in "".join(sys.argv):
     sys.modules["xformers"] = None
+    try:
+    import torchvision.transforms.functional_tensor  # noqa: F401
+except ImportError:
+    try:
+        import torchvision.transforms.functional as functional
+        sys.modules["torchvision.transforms.functional_tensor"] = functional
+    except ImportError:
+        pass  # shrug...
